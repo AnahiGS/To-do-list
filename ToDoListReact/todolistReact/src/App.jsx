cfbaa92f30c861030061f38c16000bc7c82c0a57
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-11-28 18:22:23 -0600

    added functionality

--- ToDoListReact/todolistReact/src/App.jsx
+++ ToDoListReact/todolistReact/src/App.jsx
@@ -4,14 +4,35 @@ import TaskBox from './components/TaskBox'
 import './App.css'
 
 function App () {
+  const [tasks, setTasks] = useState('')
+
+  const onAddTask = (newTask) => {
+    if (tasks.includes(newTask)) {
+      return
+    } // si el arreglo de tareas incluye la nueva categoria, entonces no agregue la tarea
+    setTasks([...tasks, newTask])
+  }
+
   return (
-    <div className="ToDoList">
-      <h1>To Do List</h1>
-      <input type='text' placeholder='Write a task' onChange={}/>
-      <div>
-        <TaskBox/>
+    <>
+      <div className="ToDoList">
+        <h1>To Do List</h1>
+
+        <TaskBox onNewTask={onAddTask} />
       </div>
-    </div>
+      <h3>List of tasks to do </h3>
+      <ul id='lista'>
+        {tasks.map((task) => {
+          return (
+            <li key={task}>
+              <i className='far fa-circle co' data='realizado' id='0' />
+              <p className='text'>{task} </p>
+              <i className='fas fa-trash de' data='eliminado' id='0' />
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
--- ToDoListReact/todolistReact/src/components/TaskBox.jsx
+++ ToDoListReact/todolistReact/src/components/TaskBox.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TaskBox = () => {
+const TaskBox = ({ onNewTask }) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value)
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const valueInput = inputValue.trim()
+
+    if (valueInput.length <= 1) return // si el largo de lo que se escriba en el input es menor o igual a 1, entonces simplemente no se va a ejecutar nada
+    onNewTask(valueInput)
+    setInputValue('')
+  }
+
   return (
-    <div>TaskBox</div>
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='Write a task'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+
   )
 }
 
